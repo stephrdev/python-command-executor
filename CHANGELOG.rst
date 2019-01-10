a505e10eef81b08324d0093b3c34f0f014985582
Stephan Jaekel <steph@rdev.info>
2019-01-10 15:23:26 +0100

    Prepare release.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+0.1.0 - 2019-01-10
+------------------
+
+* Improved support for background / forking processes.
+
+
 0.0.1 - 2018-08-08
 ------------------
 
--- command_executor/__init__.py
+++ command_executor/__init__.py
@@ -2,4 +2,4 @@ from .command import Command  # noqa
 from .exceptions import CommandError, CommandExecutionError, CommandParameterError  # noqa
 
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
